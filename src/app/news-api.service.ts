585721b7a32f4ce9ce0ae2c5036a8f4d0e293c07
titus <tkivite@gmail.com>
2019-05-22 08:54:42 +0300

    Added the news api service and the view

---
+++ src/app/news-api.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root"
+})
+export class NewsApiService {
+  api_key = "926067d7111243c0a3cd0980347dda36";
+
+  constructor(private http: HttpClient) {}
+  initSources() {
+    return this.http.get(
+      "https://newsapi.org/v2/sources?language=en&apiKey=" + this.api_key
+    );
+  }
+  initArticles() {
+    return this.http.get(
+      "https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=" +
+        this.api_key
+    );
+  }
+  getArticlesByID(source: String) {
+    return this.http.get(
+      "https://newsapi.org/v2/top-headlines?sources=" +
+        source +
+        "&apiKey=" +
+        this.api_key
+    );
+  }
+}
